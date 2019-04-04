54d944db16e1c193bbed74c69d34da4ba56774b9
Zachary <zach@csh.rit.edu>
2019-04-03 22:01:20 -0400

    default to 0.0.0.0 and 443 when not debug

--- wsgi.py
+++ wsgi.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     cert_path = '/etc/letsencrypt/live/{}.csh.rit.edu/'.format(app.config['MACHINE_NAME'])
     app.debug = app.config['DEBUG']
     if not app.debug:
-        app.run(host=app.config['IP'], 
-                port=app.config['PORT'], 
+        app.run(host="0.0.0.0", 
+                port=443, 
                 ssl_context=(cert_path+'fullchain.pem', cert_path+'privkey.pem'))
     else:
         app.run(host=app.config['IP'], port=app.config['PORT'])
