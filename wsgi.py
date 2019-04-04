2cecf1a254788f12b45a066cfcb72e7d4dbb3476
Zachary <zach@csh.rit.edu>
2019-04-03 21:51:58 -0400

    add ssl_context

--- wsgi.py
+++ wsgi.py
@@ -4,8 +4,14 @@ import os
 from potion_seller import app
 
 if __name__ == "__main__":
+    cert_path = '/etc/letsencrypt/live/{}.csh.rit.edu/'.format(app.config['MACHINE_NAME'])
     app.debug = app.config['DEBUG']
-    app.run(host=app.config['IP'], port=app.config['PORT'])
+    if not app.debug:
+        app.run(host=app.config['IP'], 
+                port=app.config['PORT'], 
+                ssl_context=(cert_path+'fullchain.pem', cert_path+'privkey.pem'))
+    else:
+        app.run(host=app.config['IP'], port=app.config['PORT'])
 
 application = app
 
