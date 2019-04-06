0389c19642941063097a5e3a462f8e227aba9096
Zachary <zach@csh.rit.edu>
2019-04-06 18:49:51 -0400

    real response json for /drop

--- potion_seller/drop.py
+++ potion_seller/drop.py
@@ -8,11 +8,43 @@ drop_bp = Blueprint('drop_bp', __name__)
 @drop_bp.route('/drop', methods=['POST'])
 @auth
 def drop_drink():
-    slot = request.get_json()['slot']
+    if request.headers.get('Content-Type') != 'application/json':
+        failure = {
+            "error": "Please ensure the content-type of your request is valid",
+            "errorCode": 400
+        }
+        return jsonify(failure), 400
+
+    try:
+        slot = int(request.get_json()['slot'])
+        if slot < 1:
+            raise ValueError
+    except ValueError:
+        failure = {
+            "error": "Ensure that the slot number is a valid, positive integer",
+            "errorCode": 400,
+        }
+        return jsonify(failure), 400
+
     if slot > len(app.config['W1_ADDRESSES']): # slot index doesn't exist
-        return jsonify('Could not find slot ' + str(slot)), 400
+        failure = {
+            "error": "The slot number provided was beyond the range of the machine (slots: {})".format(
+                len(app.config['W1_ADDRESSES'])
+            ),
+            "errorCode": 400
+        }
+
+        return jsonify(failure), 400
     elif machine_obj.drop(slot):
-        return jsonify('dropping drink at slot: ' + str(slot)), 200
+        success = {
+            "message": "Dropped drink from slot {}".format(slot)
+        }
+        return jsonify(success), 200
     else:
-        return jsonify('unable to drop drink from slot: ' + str(slot)), 400
+        failure = {
+            "error": "The slot is not mounted, available, or was busy",
+            "errorCode": 503 # Service Unavailable
+        }
+
+        return jsonify(failure), 503
 
